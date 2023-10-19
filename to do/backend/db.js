1adfd5443921f953b96833e0dd081794d5dfd7b8
Abdul Babawale <wahabbabawale@gmail.com>
2023-10-19 09:59:16 -0500

    made changes to jenkins file

---
+++ to do/backend/db.js
@@ -0,0 +1,15 @@
+const Pool = require("pg").Pool;
+const fs = require("fs");
+const pool = new Pool({
+  user: "postgres",
+  password: "postgres",
+  database: "perntodo",
+  port: 5432,
+  host: "database-1.cgcszzcgyegq.us-east-1.rds.amazonaws.com",
+ ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('us-east-1-bundle.pem').toString(),
+},  
+});
+
+module.exports = pool;
